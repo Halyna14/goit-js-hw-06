8fae657c176218c6b8deb35de1b66db35beeda3f
Halyna Melnychuk <106915592+Halyna14@users.noreply.github.com>
2022-10-25 23:57:13 +0300

    Update task-01.js

---
+++ src/js/task-01.js
@@ -0,0 +1,10 @@
+const categEl = document.querySelector('#categories');
+const categItemEl = document.querySelectorAll('.item');
+
+console.log('Number of categories:', categEl.children.length);
+
+for (let i = 0; i < categItemEl.length; i++) {
+    console.log('Category: ' + categItemEl[i].querySelector('h2').textContent);
+    console.log('Elements: ' + categItemEl[i].querySelectorAll('ul li').length);
+}
+
