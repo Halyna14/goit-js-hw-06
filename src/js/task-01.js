ab0ea49ce5b8edce6ef4b50f16fdcb4e1991b154
Halyna Melnychuk <106915592+Halyna14@users.noreply.github.com>
2022-10-29 00:29:03 +0300

    edit

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,10 +1,9 @@
-const categEl = document.querySelector('#categories');
-const categItemEl = document.querySelectorAll('.item');
+const categEl = document.querySelector("#categories");
+const categItemEl = document.querySelectorAll(".item");
 
-console.log('Number of categories:', categEl.children.length);
+console.log("Number of categories:", categEl.children.length);
 
 for (let i = 0; i < categItemEl.length; i++) {
-    console.log('Category: ' + categItemEl[i].querySelector('h2').textContent);
-    console.log('Elements: ' + categItemEl[i].querySelectorAll('ul li').length);
+  console.log("Category: " + categItemEl[i].querySelector("h2").textContent);
+  console.log("Elements: " + categItemEl[i].querySelectorAll("ul li").length);
 }
-
--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ul = document.querySelector("#ingredients");
+console.log(ul);
+const arr = [];
+for (let i = 0; i < 6; i += 1) {
+  const li = document.createElement("li");
+  li.classList.add("js-item");
+  li.textContent = ingredients[i];
+  arr.push(li);
+}
+ul.append(...arr);
