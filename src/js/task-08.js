e233e01fffb4faa29bd286115db4b414bb48982b
Halyna Melnychuk <106915592+Halyna14@users.noreply.github.com>
2022-11-01 22:22:25 +0200

    Update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,20 @@
+const formEl = document.querySelector(".login-form");
+formEl.addEventListener("submit", onFormSubmit);
+function onFormSubmit(event) {
+  event.preventDefault();
 
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    alert(`Bсі поля повинні бути заповнені`);
+  }
+  const obj = {
+    email: email.value,
+    password: password.value,
+  };
+
+  console.log(obj);
+  formEl.reset();
+}
