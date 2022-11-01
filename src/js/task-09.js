19a4e35b156324b50662d8d4551e2693fabba4bc
Halyna Melnychuk <106915592+Halyna14@users.noreply.github.com>
2022-11-01 22:31:53 +0200

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyEl = document.querySelector("body");
+const btnEl = document.querySelector(".change-color");
+const colorEl = document.querySelector(".color");
+
+btnEl.addEventListener("click", onBtnClick);
+
+function onBtnClick() {
+  colorEl.textContent = getRandomHexColor();
+  bodyEl.style.backgroundColor = colorEl.textContent;
+}
