099b335ae75593aa4e4d806adb176fe3e1c9299d
Halyna Melnychuk <106915592+Halyna14@users.noreply.github.com>
2022-11-01 16:23:13 +0200

    Update task-04.js

---
+++ src/js/task-04.js
@@ -0,0 +1,17 @@
+let counterValue = 0;
+
+
+const btnDecrement = document.querySelector('[data-action="decrement"]');
+const btnIncrement = document.querySelector('[data-action="increment"]');
+const valueEl = document.querySelector('#value');
+
+
+
+btnDecrement.addEventListener('click', () => {
+  counterValue -= 1;
+  valueEl.textContent = counterValue;
+});
+btnIncrement.addEventListener('click', () => {
+    counterValue += 1;
+    valueEl.textContent = counterValue;
+  });
